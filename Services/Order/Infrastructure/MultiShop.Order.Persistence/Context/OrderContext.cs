134f3827bc9616c99c16f3ac535ecb51d7c04e3e
sibelkrblt <sibel.karabulut.20@gmail.com>
2024-07-20 17:59:21 +0300

    Onion Architecture, CQRS Design Pattern ve Repository Pattern ile Order Microservisi hazırlandı. MSSQL veritabanı entegrasyonu yapıldı. Address ve OrderDetail için endpointler yazıldı.

---
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Persistence.Context
+{
+	public class OrderContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=LAPTOP-HF106PUQ\\SQLEXPRESS; database=MultiShopOrderDb; trusted_connection=true; TrustServerCertificate=True");
+		}
+
+		public DbSet<Address> Addresses => Set<Address>();
+		public DbSet<OrderDetail> OrderDetails => Set<OrderDetail>();
+		public DbSet<Ordering> Orderings => Set<Ordering>();
+	}
+}
